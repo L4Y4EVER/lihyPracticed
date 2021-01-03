57a195d039a8cd7b47a5d5ac8c2b2259b5d322f5
lihy <lihongyan@koalareading.com>
2021-01-03 20:30:43 +0800

    提交新增每日练习代码

---
+++ leetcodePracticed/src/com/lihy/practiced/twentytwentyone/JanuaryPracticed.java
@@ -0,0 +1,113 @@
+package com.lihy.practiced.twentytwentyone;
+
+import com.lihy.practiced.twentytwentyone.bean.ListNode;
+
+/**
+ * 一月每日一题
+ *
+ * @author lhy
+ * @date 2021/1/3
+ */
+public class JanuaryPracticed {
+
+    /**
+     * 0101
+     *
+     * @param flowerbed 入参数组
+     * @param n 位置
+     * @return 结果
+     */
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int count = 0;
+        int m = flowerbed.length;
+        int prev = -1;
+        for (int i = 0; i < m; i++) {
+            if (flowerbed[i] == 1) {
+                if (prev < 0) {
+                    count += i / 2;
+                } else {
+                    count += (i - prev - 2) / 2;
+                }
+                prev = i;
+            }
+        }
+        if (prev < 0) {
+            count += (m + 1) / 2;
+        } else {
+            count += (m - prev - 1) / 2;
+        }
+        return count >= n;
+    }
+
+
+    /**
+     * 0102
+     *
+     * 给你一个整数数组 nums，有一个大小为k的滑动窗口从数组的最左侧移动到数组的最右侧。
+     * 你只可以看到在滑动窗口内的 k个数字。滑动窗口每次只向右移动一位。
+     *
+     * 返回滑动窗口中的最大值。
+     *
+     * @param nums 数组
+     * @param k k
+     * @return 结果
+     */
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] prefixMax = new int[n];
+        int[] suffixMax = new int[n];
+        for (int i = 0; i < n; ++i) {
+            if (i % k == 0) {
+                prefixMax[i] = nums[i];
+            }
+            else {
+                prefixMax[i] = Math.max(prefixMax[i - 1], nums[i]);
+            }
+        }
+        for (int i = n - 1; i >= 0; --i) {
+            if (i == n - 1 || (i + 1) % k == 0) {
+                suffixMax[i] = nums[i];
+            } else {
+                suffixMax[i] = Math.max(suffixMax[i + 1], nums[i]);
+            }
+        }
+
+        int[] ans = new int[n - k + 1];
+        for (int i = 0; i <= n - k; ++i) {
+            ans[i] = Math.max(suffixMax[i], prefixMax[i + k - 1]);
+        }
+        return ans;
+    }
+
+
+    /**
+     * 0103
+     *
+     * 给你一个链表和一个特定值 x ，请你对链表进行分隔，使得所有小于 x 的节点都出现在大于或等于 x 的节点之前。
+     *
+     * 你应当保留两个分区中每个节点的初始相对位置。
+     *
+     * @param head 头结点
+     * @param x 分割地址
+     * @return 返回链条
+     */
+    public ListNode partition(ListNode head, int x) {
+        ListNode small = new ListNode(0);
+        ListNode smallHead = small;
+        ListNode large = new ListNode(0);
+        ListNode largeHead = large;
+        while (head != null) {
+            if (head.val < x) {
+                small.next = head;
+                small = small.next;
+            } else {
+                large.next = head;
+                large = large.next;
+            }
+            head = head.next;
+        }
+        large.next = null;
+        small.next = largeHead.next;
+        return smallHead.next;
+    }
+}
---
+++ leetcodePracticed/src/com/lihy/practiced/twentytwentyone/bean/ListNode.java
@@ -0,0 +1,13 @@
+package com.lihy.practiced.twentytwentyone.bean;
+
+/**
+ * @author lhy
+ * @date 2021/1/3
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int x){
+        val = x;
+    }
+}
