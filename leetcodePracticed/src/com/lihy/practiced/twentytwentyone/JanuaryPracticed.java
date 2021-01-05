22f7eec51bd259b2b499bfcfc706cbbf0cb3c54f
lihy <lihongyan@koalareading.com>
2021-01-05 23:35:49 +0800

    提交0105 代码

--- leetcodePracticed/src/com/lihy/practiced/twentytwentyone/JanuaryPracticed.java
+++ leetcodePracticed/src/com/lihy/practiced/twentytwentyone/JanuaryPracticed.java
@@ -2,6 +2,10 @@ package com.lihy.practiced.twentytwentyone;
 
 import com.lihy.practiced.bean.ListNode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * 一月每日一题
  *
@@ -9,6 +13,38 @@ import com.lihy.practiced.bean.ListNode;
  * @date 2021/1/3
  */
 public class JanuaryPracticed {
+
+    /**
+     * 0105
+     * 在一个由小写字母构成的字符串 s 中，包含由一些连续的相同字符所构成的分组。
+     *
+     * 例如，在字符串 s = "abbxxxxzyy"中，就含有 "a", "bb", "xxxx", "z" 和 "yy" 这样的一些分组。
+     *
+     * 分组可以用区间 [start, end] 表示，其中 start 和 end 分别表示该分组的起始和终止位置的下标。上例中的 "xxxx" 分组用区间表示为 [3,6] 。
+     *
+     * 我们称所有包含大于或等于三个连续字符的分组为 较大分组 。
+     *
+     * 找到每一个 较大分组 的区间，按起始位置下标递增顺序排序后，返回结果。
+     *
+     * @param s 字符串
+     * @return 结果
+     */
+    public List<List<Integer>> largeGroupPositions(String s) {
+        List<List<Integer>> lists = new ArrayList<>();
+        int n = s.length();
+        int num = 0;
+        for (int i = 0; i < n; i++){
+            if (i == n - 1 || s.charAt(i) != s.charAt(i + 1)) {
+                if (num >= 3) {
+                    lists.add(Arrays.asList(i - num + 1, i));
+                }
+                num = 1;
+            } else {
+                num++;
+            }
+        }
+        return lists;
+    }
 
     /**
      * 0104
