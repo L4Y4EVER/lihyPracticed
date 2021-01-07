1290eeda981d07c15b6c72d779b25380614b4283
lihy <lihongyan@koalareading.com>
2021-01-07 23:08:10 +0800

    提交0107 代码

--- leetcodePracticed/src/com/lihy/practiced/twentytwentyone/JanuaryPracticed.java
+++ leetcodePracticed/src/com/lihy/practiced/twentytwentyone/JanuaryPracticed.java
@@ -13,6 +13,45 @@ import java.util.*;
 public class JanuaryPracticed {
 
     /**
+     * 0107
+     *
+     * 有 n 个城市，其中一些彼此相连，另一些没有相连。如果城市 a 与城市 b 直接相连，且城市 b 与城市 c 直接相连，那么城市 a 与城市 c 间接相连。
+     *
+     * 省份 是一组直接或间接相连的城市，组内不含其他没有相连的城市。
+     *
+     * 给你一个 n x n 的矩阵 isConnected ，其中 isConnected[i][j] = 1 表示第 i 个城市和第 j 个城市直接相连，
+     * 而 isConnected[i][j] = 0 表示二者不直接相连。
+     *
+     * 返回矩阵中 省份 的数量。
+
+     *
+     * @param isConnected 数组
+     * @return 结果
+     */
+    public int findCircleNum(int[][] isConnected) {
+        int provinces = isConnected.length;
+        boolean[] visited = new boolean[provinces];
+        int circles = 0;
+        for (int i = 0; i < provinces; i++) {
+            if (!visited[i]) {
+                dfs(isConnected, visited, provinces, i);
+                circles++;
+            }
+        }
+        return circles;
+    }
+
+    public void dfs(int[][] isConnected, boolean[] visited, int provinces, int i) {
+        for (int j = 0; j < provinces; j++) {
+            if (isConnected[i][j] == 1 && !visited[j]) {
+                visited[j] = true;
+                dfs(isConnected, visited, provinces, j);
+            }
+        }
+    }
+
+
+    /**
      * 0106
      * 给你一个变量对数组 equations 和一个实数值数组 values 作为已知条件，其中 equations[i] = [Ai, Bi] 和 values[i]
      * 共同表示等式 Ai / Bi = values[i] 。每个 Ai 或 Bi 是一个表示单个变量的字符串。
