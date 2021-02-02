3bfbc19e6a50714c7bcb3e777f7d7b3139b061e4
lihy <lihongyan@koalareading.com>
2021-02-02 23:43:20 +0800

    添加数组处理代码

--- leetcodePracticed/src/com/lihy/practiced/interview/ArrayPractice.java
+++ leetcodePracticed/src/com/lihy/practiced/interview/ArrayPractice.java
@@ -33,4 +33,21 @@ public class ArrayPractice {
             System.arraycopy(nums2,p2, nums1,p1 + p2,m+n -p1 -p2);
         }
     }
+
+    public void mergePlus(int[] nums1, int m, int[] nums2, int n) {
+
+        int p = m + n - 1;
+        int p1 = m-1;
+        int p2 = n-1;
+
+        while ( p1 >= 0 && p2 >= 0) {
+            nums1[p--] = nums1[p1] > nums2[p2] ? nums1[p1--] : nums2[p2--];
+        }
+
+        if ( n > 0){
+            System.arraycopy(nums2,0,nums1,0,p + 1);
+        }
+
+    }
+
 }
