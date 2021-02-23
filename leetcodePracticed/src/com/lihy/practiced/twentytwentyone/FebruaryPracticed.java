d7161b0434254bf519db0f80e311bae54264efb0
lihy <lihongyan@koalareading.com>
2021-02-23 18:27:03 +0800

    含泪再写一遍反转链表

--- leetcodePracticed/src/com/lihy/practiced/twentytwentyone/FebruaryPracticed.java
+++ leetcodePracticed/src/com/lihy/practiced/twentytwentyone/FebruaryPracticed.java
@@ -1,5 +1,7 @@
 package com.lihy.practiced.twentytwentyone;
 
+import com.lihy.practiced.bean.ListNode;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -13,8 +15,41 @@ public class FebruaryPracticed {
 
     public static void main(String[] args) {
 
+        ListNode head = new ListNode(1);
+        ListNode node1 = new ListNode(2);
+        ListNode node2 = new ListNode(3);
 
 
+        head.next = node1;
+        node1.next = node2;
+
+        ListNode listNode = reListNode(head);
+        System.out.println(listNode.val);
+
+        ListNode perNode = null;
+        ListNode curNode = listNode;
+
+        while (curNode != null){
+            ListNode next = curNode.next;
+            curNode.next = perNode;
+            perNode = curNode;
+            curNode = next;
+        }
+
+        System.out.println(perNode.val);
+    }
+
+    private static ListNode reListNode(ListNode head) {
+        // 遍历到最后一个节点
+        if (head.next == null){
+            return head;
+        }
+        // 获取最后一个节点
+        ListNode listNode = reListNode(head.next);
+        head.next.next = head;
+        head.next = null;
+
+        return listNode;
     }
 
     public  static void printCharNum(){
