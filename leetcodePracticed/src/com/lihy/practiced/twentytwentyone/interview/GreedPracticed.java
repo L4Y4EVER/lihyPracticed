42a73b3ea20ff75106426590411c2cbbb3e8e8fd
lihy <lihongyan@koalareading.com>
2021-03-01 20:54:24 +0800

    简单的贪心算法

--- leetcodePracticed/src/com/lihy/practiced/twentytwentyone/interview/GreedPracticed.java
+++ leetcodePracticed/src/com/lihy/practiced/twentytwentyone/interview/GreedPracticed.java
@@ -1,5 +1,7 @@
 package com.lihy.practiced.twentytwentyone.interview;
 
+import java.util.Arrays;
+
 /**
  * 贪心算法学习
  *
@@ -8,11 +10,34 @@ package com.lihy.practiced.twentytwentyone.interview;
  */
 public class GreedPracticed {
 
-    
 
-        int findContentChildren(int[] g,int[] s){
+    /**
+     * 455
+     *
+     * 贪心指数，饼干大小
+     * @param g 贪心指数
+     * @param s 饼干大小
+     * @return 满足人数
+     */
+    int findContentChildren(int[] g,int[] s){
 
-            return 0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int gi = g.length - 1;
+        int si = s.length - 1;
+        int res = 0;
+
+        while (gi >= 0 && si >= 0){
+            if (g[gi] <= s[si]){
+                gi --;
+                si --;
+                res ++;
+            }else {
+                gi --;
+            }
         }
+        return res;
+    }
 
 }
