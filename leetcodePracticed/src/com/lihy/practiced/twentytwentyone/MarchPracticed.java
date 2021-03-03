064fbc038eb4b8f4c2ad468e7bfe40ba8d158e89
lihy <lihongyan@koalareading.com>
2021-03-03 22:28:29 +0800

    leetcode 題目

--- leetcodePracticed/src/com/lihy/practiced/twentytwentyone/MarchPracticed.java
+++ leetcodePracticed/src/com/lihy/practiced/twentytwentyone/MarchPracticed.java
@@ -8,10 +8,37 @@ package com.lihy.practiced.twentytwentyone;
  */
 public class MarchPracticed {
 
+
+    public static void main(String[] args) {
+        MarchPracticed marchPracticed = new MarchPracticed();
+        marchPracticed.countBits(2);
+    }
+
     public int[] countBits(int num) {
 
         int[] memo = new int[num + 1];
+        for (int i = num; num >0; i--){
+            String s = intToBinary(i);
+            int count = 0;
+            for (int j = 0;j < s.length(); j++){
+                if (s.charAt(j) == '1'){
+                    count++;
+                }
+            }
+            memo[i] = count;
+        }
+
+        return memo;
+    }
 
 
+
+    public String intToBinary(int n){
+        String  str = "";
+        while(n!=0) {
+            str = n % 2 + str;
+            n = n / 2;
+        }
+        return str;
     }
 }
