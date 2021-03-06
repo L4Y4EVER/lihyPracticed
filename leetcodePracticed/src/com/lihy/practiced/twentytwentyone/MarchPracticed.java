0c4d7e499da8ac62c32f9419d5798da9d4b21405
lihy <lihongyan@koalareading.com>
2021-03-06 22:48:07 +0800

    leetcode 題目

--- leetcodePracticed/src/com/lihy/practiced/twentytwentyone/MarchPracticed.java
+++ leetcodePracticed/src/com/lihy/practiced/twentytwentyone/MarchPracticed.java
@@ -19,6 +19,27 @@ public class MarchPracticed {
     }
 
     /**
+     * 03-06
+     *
+     * @param nums
+     * @return
+     */
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] ret = new int[n];
+        Arrays.fill(ret, -1);
+        Deque<Integer> stack = new LinkedList<Integer>();
+        for (int i = 0; i < n * 2 - 1; i++) {
+            while (!stack.isEmpty() && nums[stack.peek()] < nums[i % n]) {
+                ret[stack.pop()] = nums[i % n];
+            }
+            stack.push(i % n);
+        }
+        return ret;
+    }
+
+
+    /**
      * 03-05
      * 两个栈实现一个队列
      */
