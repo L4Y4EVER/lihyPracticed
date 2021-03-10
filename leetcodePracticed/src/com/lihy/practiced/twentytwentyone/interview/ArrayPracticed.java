bb4df8caac8471b6be94f1c28d4110ed1f64e08a
lihy <m13943206817@163.com>
2021-03-10 15:01:59 +0800

    提交数组操作方法

--- leetcodePracticed/src/com/lihy/practiced/twentytwentyone/interview/ArrayPracticed.java
+++ leetcodePracticed/src/com/lihy/practiced/twentytwentyone/interview/ArrayPracticed.java
@@ -48,6 +48,63 @@ public class ArrayPracticed {
         }
     }
 
+    /**
+     * 在指定元素中移除指定值并返回新数组的长度
+     * 便利一次数组，用另一个数组接收，用到了O(n)的时间复杂度
+     *
+     * 便利一次数组，将非等值的元素维护在数组前端，索引递增，
+     * 索引维护到的位置也就是所有非等值元素
+     *
+     * @param nums 目标数组
+     * @param val 指定值
+     * @return 新数组长度
+     */
+    public int removeElement(int[] nums, int val) {
+        if (nums.length == 0){
+            return 0;
+        }
+
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val){
+                if (i != k){
+                  int temp = nums[k];
+                  nums[k] = nums[i];
+                  nums[i] = temp;
+                }
+                k++;
+            }
+        }
+        return k;
+    }
+
+    /**
+     * 删除重复数值
+     * 根据有序数组的特性，遍历的过程中将k之前的数都是不同的数
+     * 0 0 1 1 2 3
+     * 维护k 个元素的数组，主要是拿第k 个元素为判断不同的标准，也就是其实该数组的长度最后要加1
+     * 始终是由k 开始，i 为第二个元素进行比较
+     *
+     * @param nums nums
+     * @return 删除后的数组长度
+     */
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0){
+            return 0;
+        }
+
+        int k = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[k]){
+                if (k != i){
+                    k++;
+                    nums[k] = nums[i];
+                }
+            }
+        }
+
+        return k + 1;
+    }
 
 
 
