04367a04a688dd58efd62dc6a48b4c1bf85b3882
lihy <m13943206817@163.com>
2021-03-23 14:25:06 +0800

    提交23 日题

--- leetcodePracticed/src/com/lihy/practiced/twentytwentyone/MarchPracticed.java
+++ leetcodePracticed/src/com/lihy/practiced/twentytwentyone/MarchPracticed.java
@@ -849,3 +849,54 @@ class ParkingSystem {
         return false;
     }
 }
+
+interface NestedInteger {
+
+    // @return true if this NestedInteger holds a single integer, rather than a nested list.
+    public boolean isInteger();
+
+    // @return the single integer that this NestedInteger holds, if it holds a single integer
+    // Return null if this NestedInteger holds a nested list
+    public Integer getInteger();
+
+    // @return the nested list that this NestedInteger holds, if it holds a nested list
+    // Return null if this NestedInteger holds a single integer
+    public List<NestedInteger> getList();
+ }
+
+
+class NestedIterator implements Iterator<Integer> {
+
+    private Deque<Iterator<NestedInteger>> stack;
+
+    public NestedIterator(List<NestedInteger> nestedList) {
+        stack = new LinkedList<>();
+        stack.push(nestedList.iterator());
+    }
+
+    @Override
+    public Integer next() {
+        return stack.peek().next().getInteger();
+    }
+
+    @Override
+    public boolean hasNext() {
+        while (!stack.isEmpty()){
+            Iterator<NestedInteger> it = stack.peek();
+            if (!it.hasNext()) { // 遍历到当前列表末尾，出栈
+                stack.pop();
+                continue;
+            }
+            // 若取出的元素是整数，则通过创建一个额外的列表将其重新放入栈中
+            NestedInteger nest = it.next();
+            if (nest.isInteger()) {
+                List<NestedInteger> list = new ArrayList<>();
+                list.add(nest);
+                stack.push(list.iterator());
+                return true;
+            }
+            stack.push(nest.getList().iterator());
+        }
+        return false;
+    }
+}
