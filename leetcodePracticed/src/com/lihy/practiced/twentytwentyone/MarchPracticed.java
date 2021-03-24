df81ed4244bd03ecb02b3c07a52b02e068a265c6
lihy <m13943206817@163.com>
2021-03-24 10:24:26 +0800

    提交24 号代码

--- leetcodePracticed/src/com/lihy/practiced/twentytwentyone/MarchPracticed.java
+++ leetcodePracticed/src/com/lihy/practiced/twentytwentyone/MarchPracticed.java
@@ -23,6 +23,47 @@ public class MarchPracticed {
         int i = evalRPN(tokens);
     }
 
+
+    /**
+     * 24 132 问题
+     * index  1， 2， 3
+     * nums[1] < nums[3] < nums[2]
+     *
+     * @param nums 数组
+     * @return 结果
+     */
+    public boolean find132pattern(int[] nums) {
+        int n = nums.length;
+        if (n < 3 ){
+            return false;
+        }
+
+        // 左侧最小值
+        int leftMin = nums[0];
+        // 右侧所有元素
+        TreeMap<Integer, Integer> rightAll = new TreeMap<Integer, Integer>();
+
+        for (int k = 2; k < n; ++k) {
+            rightAll.put(nums[k], rightAll.getOrDefault(nums[k], 0) + 1);
+        }
+
+        for (int j = 1; j < n - 1; ++j) {
+            if (leftMin < nums[j]) {
+                Integer next = rightAll.ceilingKey(leftMin + 1);
+                if (next != null && next < nums[j]) {
+                    return true;
+                }
+            }
+            leftMin = Math.min(leftMin, nums[j]);
+            rightAll.put(nums[j + 1], rightAll.get(nums[j + 1]) - 1);
+            if (rightAll.get(nums[j + 1]) == 0) {
+                rightAll.remove(nums[j + 1]);
+            }
+        }
+
+        return false;
+    }
+
     /**
      * 22日问题，返回1的个数
      *
