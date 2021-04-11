b930185473c48e7f79b8773fda9ed19256119f9b
lihy <m13943206817@163.com>
2021-04-11 11:07:18 +0800

    提交4月10日每日一题

--- leetcodePracticed/src/com/lihy/practiced/twentytwentyone/AprilPracticed.java
+++ leetcodePracticed/src/com/lihy/practiced/twentytwentyone/AprilPracticed.java
@@ -14,6 +14,32 @@ public class AprilPracticed {
         System.out.println(clumsy(10));;
     }
 
+    /**
+     * 11 号题
+     *
+     * @param n 入参
+     * @return 结果
+     */
+    public int nthUglyNumber(int n) {
+        int[] factors = {2, 3, 5};
+        Set<Long> seen = new HashSet<Long>();
+        PriorityQueue<Long> heap = new PriorityQueue<Long>();
+        seen.add(1L);
+        heap.offer(1L);
+        int ugly = 0;
+        for (int i = 0; i < n; i++) {
+            long curr = heap.poll();
+            ugly = (int) curr;
+            for (int factor : factors) {
+                long next = curr * factor;
+                if (seen.add(next)) {
+                    heap.offer(next);
+                }
+            }
+        }
+        return ugly;
+    }
+
 
     /**
      * 10 日问题
