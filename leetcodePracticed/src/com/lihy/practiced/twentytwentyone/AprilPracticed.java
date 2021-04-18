dd2bed42cbe5c538b215d749704a49c58fdcc34c
lihy <lihongyan@koalareading.com>
2021-04-18 22:28:00 +0800

    leetcode 題目

--- leetcodePracticed/src/com/lihy/practiced/twentytwentyone/AprilPracticed.java
+++ leetcodePracticed/src/com/lihy/practiced/twentytwentyone/AprilPracticed.java
@@ -19,6 +19,24 @@ public class AprilPracticed {
     }
 
     /**
+     * 18号问题
+     *
+     * @param nums 数组
+     * @return 结果
+     */
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+
+        for (int j =1; j < nums.length; j ++ ){
+            if (nums[i] != nums[j]){
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+
+    /**
      * 17 号问题
      *
      * @param nums 数组
